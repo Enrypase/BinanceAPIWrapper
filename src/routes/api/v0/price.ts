ae81fa8df386bec0de6bb11eb2d1142cf816370f
Enrypase <pasettoenrico02@gmail.com>
2024-10-02 19:07:55 +0200

    fix: removed , from return

--- src/routes/api/v0/price.ts
+++ src/routes/api/v0/price.ts
@@ -17,7 +17,7 @@ route.GET = async ({ searchParams }) => {
       "symbol"
     )}`
   );
-  return new Response(`Data:${data.data.price.replace(".", ",")}`);
+  return new Response(`Data:${data.data.price}`);
 };
 
 export const { GET } = route;
